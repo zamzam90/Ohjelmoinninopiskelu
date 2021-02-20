b92d20045134228f864bcb741283c002f9dbec88
zamzam90 <siltanen.sami@hotmail.com>
2021-02-20 22:30:52 +0200

    koodaushaaste 1

---
+++ 2303/js/koodi.js
@@ -0,0 +1,13 @@
+//parse some linter errors...
+/*global console*//* eslint no-console: "off" */
+/*global prompt*//* eslint no-prompt: "off" */
+/*global document*//* eslint no-document: "off" */
+/*global window*//* eslint no-window: "off" */
+/*global undef*//* eslint no-undef: "off" */
+/*global unused-var*//* eslint no-unused-vars: "off" */
+
+//funktio joka tekee ponnahdusikkunan jossa teksti "heippa maailma"
+function tervehdys() {
+    alert("Heippa");
+    alert("Maailma!");
+}
