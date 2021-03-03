a57b7bba124b76ed200f2deea090e994cf5c4769
zamzam90 <siltanen.sami@hotmail.com>
2021-03-03 11:57:04 +0200

    Seuraavat koodihaasteet, pohja.

---
+++ 0203/js/koodi.js
@@ -0,0 +1,9 @@
+//filter some linter errors...
+/*global console*//* eslint no-console: "off" */
+/*global prompt*//* eslint no-prompt: "off" */
+/*global document*//* eslint no-document: "off" */
+/*global window*//* eslint no-window: "off" */
+/*global undef*//* eslint no-undef: "off" */
+/* eslint no-unused-vars: "off" */
+
+// 1.Tehtävä
