9b312d1e1bcca82023fb3a168fdabc9a71b9d86b
zamzam90 <siltanen.sami@hotmail.com>
2021-03-12 12:53:01 +0200

    viikon2 perustehtävät, toinen aloitettu.

--- Perustehtäviä_vko2_vko3/Vko2/Toka/js/koodi.js
+++ Perustehtäviä_vko2_vko3/Vko2/Toka/js/koodi.js
@@ -5,5 +5,34 @@
 /*global window*//* eslint no-window: "off" */
 /*global undef*//* eslint no-undef: "off" */
 /* eslint no-unused-vars: "off" */
-
+console.log("Tässä tokan viikon tokat tehtävät..");
 //Vko 2 perustehtäviä, tokat.
+//1.tehtävä
+function ehto2lauseita1() {
+  //haetaan käyttäjän antama luku htmlstä
+  nmbr = document.getElementById('joqLuku').value;
+  //tarkistetaan inko luku positiivinen vai negatiivinen
+  //ilmoitetaan alertilla kumpi luku on..
+  if (nmbr < 0) {
+    alert("negatiivinen");
+    //kirjoitetaan vastaus vielä htmlään myös..
+    document.getElementById('vastaus1').innerHTML = "Antamasi luku: " +  nmbr + ", on negatiivinen.";
+  } else {
+    alert("positiivinen");
+    //kirjoitetaan vastaus vielä htmlään myös..
+    document.getElementById('vastaus1').innerHTML = "Antamasi luku: " +  nmbr + ", on positiivinen.";
+  }
+}
+
+//reset answer paragraphs..
+function empty()
+{
+  //tyhjätään KAIKKI..
+  document.getElementById("joqLuku").value = "";
+  document.getElementById("vastaus1").innerHTML = "Vastaus..";
+  document.getElementById("vastaus2").innerHTML = "Vastaus..";
+  document.getElementById("vastaus3").innerHTML = "Vastaus..";
+  document.getElementById("vastaus4").innerHTML = "Vastaus..";
+  document.getElementById("vastaus5").innerHTML = "Vastaus..";
+}
+console.log("Programmed by Sami.S KKTI20C - 2021");
