546805451e254f3101e0563e3f280b68af78b597
zamzam90 <siltanen.sami@hotmail.com>
2021-03-16 16:43:52 +0200

    vko2 perustehtävät, toka, 1-3 done, kesken.

--- Perustehtäviä_vko2_vko3/Vko2/Eka/js/koodi.js
+++ Perustehtäviä_vko2_vko3/Vko2/Eka/js/koodi.js
@@ -15,17 +15,17 @@ console.log("Viikon2 ekat perustehtävät.");
 //1.Tehtävä, pyydetään 3 numeroa ja laitetaan ne järjestykseen..
 function sortEm() {
   //hae numerot htmlstä
-  uno = document.getElementById('first').value;
-  dos = document.getElementById('second').value;
-  tres = document.getElementById('third').value;
+  var uno = document.getElementById('first').value;
+  var dos = document.getElementById('second').value;
+  var tres = document.getElementById('third').value;
   //lisää numerot taulukkoon
-  numerot = [uno, dos, tres];
+  var numerot = [uno, dos, tres];
   //järkkää taulukkoon
   numerot.sort(function(a, b) {
     return a - b;
   });
   //siivotaan pilkut pois..
-  selkee = numerot.join(" ").toLocaleString();
+  var selkee = numerot.join(" ").toLocaleString();
   //anna vastais alerttina?
   alert("Annoit numerot: " + uno + "," + dos + "," + tres + "\n" + "Pienimmästä suurimpaan: " + selkee);
   //kirjoitetaan vastaus myös htmlään..
@@ -35,19 +35,19 @@ function sortEm() {
 //get numbers from html..
 function biggest() {
   //get numbers from html form..
-  uno = document.getElementById('first_number').value;
-  dos = document.getElementById('second_number').value;
-  tres = document.getElementById('third_number').value;
-  cuatro = document.getElementById('fourth_number').value;
-  cinco = document.getElementById('fifth_number').value;
+  var uno = document.getElementById('first_number').value;
+  var dos = document.getElementById('second_number').value;
+  var tres = document.getElementById('third_number').value;
+  var cuatro = document.getElementById('fourth_number').value;
+  var cinco = document.getElementById('fifth_number').value;
   //put them in an array and sort it (w3schools helped with sort..)
-  numberss = [uno, dos, tres, cuatro, cinco];
-  numbers = [uno, dos, tres, cuatro, cinco];
-  sortedNumbers = numbers.sort(function(a, b) {
+  var numberss = [uno, dos, tres, cuatro, cinco];
+  var numbers = [uno, dos, tres, cuatro, cinco];
+  var sortedNumbers = numbers.sort(function(a, b) {
     return b - a;
   });
   //get the last number in the array a.k.a the biggest
-  theBiggest = sortedNumbers[0];
+  var theBiggest = sortedNumbers[0];
   alert("Annoit numerot: " + numberss + "\n" + "Suurin on: " +theBiggest);
   //kirjoitetaan vastaus myös htmlään..
   document.getElementById("vastaus2").innerHTML = "Annoit numerot: " + numberss + "\n" + "Suurin on: " +theBiggest;
@@ -55,7 +55,7 @@ function biggest() {
 //3. Mission, is the number even or odd?
 function evnORodd() {
   //hajetaan luku htmlstä..
-  luku = document.getElementById('luku').value;
+  var luku = document.getElementById('luku').value;
   //if parillinen do this, else do that..
   if (luku % 2 === 0) {
     alert("Annoit luvun " + luku + ", se on parillinen.");
@@ -70,7 +70,7 @@ function evnORodd() {
 //4.Tehtävä, annetaan ajoneuvo iän mukaan..
 function reteli() {
   //haetaan ikä htmlstä..
-  age = document.getElementById('ika').value;
+  var age = document.getElementById('ika').value;
   //jos <16 fillari, jos <18 mopo, muuten auto.
   if (age < 16) {
     alert("Olet " + age + " vuotta vanha, saat ajaa polkupyörää.");
@@ -88,7 +88,7 @@ function reteli() {
 //5. Tehtävä, näytetään "hei maailma" valitulla kielellä..
 function heippaMaailma() {
   //haetaan käyttäjän valinta htmlstä..
-  x = document.getElementById('select_lang').value;
+  var x = document.getElementById('select_lang').value;
   alert(x);
   //kirjoitetaan vastaus myös htmlään..
   document.getElementById("vastaus5").innerHTML = x;
--- Perustehtäviä_vko2_vko3/Vko2/Toka/js/koodi.js
+++ Perustehtäviä_vko2_vko3/Vko2/Toka/js/koodi.js
@@ -8,9 +8,10 @@
 console.log("Tässä tokan viikon tokat tehtävät..");
 //Vko 2 perustehtäviä, tokat.
 //1.tehtävä
+//Onko luku positiivinen vai negatiivinen?
 function ehto2lauseita1() {
   //haetaan käyttäjän antama luku htmlstä
-  nmbr = document.getElementById('joqLuku').value;
+  var nmbr = document.getElementById('joqLuku').value;
   //tarkistetaan onko luku positiivinen vai negatiivinen
   //ilmoitetaan alertilla vastaus..
   if (nmbr < 0) {
@@ -25,6 +26,65 @@ function ehto2lauseita1() {
     document.getElementById('vastaus1').innerHTML = "Antamasi luku: " +  nmbr + ", on positiivinen.";
   }
 }
+//2.Tehtävä,
+//viikonpäivä numero..
+function viikonPäiväNro() {
+  //haetaan htmlstä käyttäjän antama numero.
+  var päiväNro = parseInt(document.getElementById('pvNro').value);
+  var päiväOn = "joQpäivä";
+  switch(päiväNro) {
+    case 1:
+    päiväOn = "Maanantai";
+    break;
+    case 2:
+    päiväOn = "Tiistai";
+    break;
+    case 3:
+    päiväOn = "Keskiviikko";
+    break;
+    case 4:
+    päiväOn = "Torstai";
+    break;
+    case 5:
+    päiväOn = "Perjantai";
+    break;
+    case 6:
+    päiväOn = "Lauantai";
+    break;
+    default:
+    päiväOn = "Sunnuntai";
+    break;
+  }
+  alert(päiväOn);
+  document.getElementById('vastaus2').innerHTML = "Antamasi numero:" + päiväNro + ", on " + päiväOn + ".";
+}
+//3.Tehtävä
+//tarkastetaan onko käyttäjän antama vuosluku karkausvuosi.
+function karkausVuosiko() {
+  console.log("lasketaan onko vuosiluku karkausvuosi:");
+  var karkaus = "karkaako";
+  var vuosiluku = document.getElementById('vuosiLuku').value;
+  if (vuosiluku % 4 == 0 && vuosiluku % 100 !== 0 ) {
+    karkaus = " on karkausvuosi.";
+    console.log("vuosi on jaollinen neljällä sekä ei sadalla=karkausvuosi.");
+    alert("Vuosi on karkausvuosi");
+  } else if (vuosiluku % 400 == 0) {
+    karkaus = " on karkausvuosi.";
+    console.log("Vuosi on jaollinen neljälläsadalla=karkausvuosi.");
+    alert("Vuosi on karkausvuosi");
+  } else {
+    karkaus = " ei ole karkausvuosi.";
+    console.log("vuosi ei ole karkausvuosi.");
+    alert("Vuosi ei ole karkausvuosi");
+  }
+  document.getElementById('vastaus3').innerHTML = "Antamasi vuosi: " + vuosiluku + ", " + karkaus;
+}
+//4.Tehtävä
+//kysytään käyttäjältä 5 lukua, lasketaan niiden summa ja keskiarvo.
+function summaJaKA() {
+  
+}
+
 
 //reset answer paragraphs..
 function empty()
