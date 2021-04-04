f5c352bbdbd9b14dfbf5bf9adf4e3130a556ab39
zamzam90 <siltanen.sami@hotmail.com>
2021-04-04 17:54:14 +0300

    vko3, eka tehtävä aloitettu

--- Perustehtäviä_vko2_vko3/Vko3/Toka/js/koodi.js
+++ Perustehtäviä_vko2_vko3/Vko3/Toka/js/koodi.js
@@ -7,9 +7,25 @@
 /* eslint no-unused-vars: "off" */
 console.log("Tässä kolmosviikon tokat tehtävät..");
 //Vko 3 perustehtäviä, tokat.
+//1.Tehtävä
+//lasketaan sanalle pisteet kirjainten perusteella..
+function laskeSanaPisteet() {
+  var scrabbleSana = "", sanaTaulukoksi, pisteet=0;
+  scrabbleSana = document.getElementById("scrabbleSana").value;
+  console.log(scrabbleSana);
+
+}
+
+//2.Tehtävä
+//arvotaan lottonumerot..
 
 
+//3.Tehtävä
+//muunnetaan ennalta määritelty js taulukko html taulukkomuotoon..
 
+
+//4.Tehtävä
+//tehdään taulukko kaikista korttipakankorteista, arvotaan sieltä 5korttia..
 
 
 
