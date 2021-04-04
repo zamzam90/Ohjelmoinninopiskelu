0af77d600de14335424727ed321484c8fc33bab9
zamzam90 <siltanen.sami@hotmail.com>
2021-04-04 17:40:07 +0300

    vko3 tokat tehävät pohjia..

--- Perustehtäviä_vko2_vko3/Vko3/Toka/js/koodi.js
+++ Perustehtäviä_vko2_vko3/Vko3/Toka/js/koodi.js
@@ -5,5 +5,25 @@
 /*global window*//* eslint no-window: "off" */
 /*global undef*//* eslint no-undef: "off" */
 /* eslint no-unused-vars: "off" */
-
+console.log("Tässä kolmosviikon tokat tehtävät..");
 //Vko 3 perustehtäviä, tokat.
+
+
+
+
+
+
+//tyhjätään KAIKKI..
+function empty() {
+  //document.getElementById("").value = "";
+  //document.getElementById("").value = "";
+  //document.getElementById("").value = "";
+  //document.getElementById("").value = "";
+  //document.getElementById("").value = "";
+  //document.getElementById("vastaus1").innerHTML = "Parilliset luvut: ";
+  //document.getElementById("vastaus2").innerHTML = "Salasana muunnos: ";
+  //document.getElementById("vastaus3").innerHTML = " ";
+  //document.getElementById("vastaus4").innerHTML = "Vastaus..";
+
+}
+console.log("Programmed by Sami.S KKTI20C - 2021");
