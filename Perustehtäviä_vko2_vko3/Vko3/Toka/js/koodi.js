8a24e912a63c3673988314dfb37fa1364a35a8ae
zamzam90 <siltanen.sami@hotmail.com>
2021-04-09 13:12:10 +0300

    vko3 tokat, toka tehtävä valmis

--- Perustehtäviä_vko2_vko3/Vko3/Toka/js/koodi.js
+++ Perustehtäviä_vko2_vko3/Vko3/Toka/js/koodi.js
@@ -65,14 +65,45 @@ function laskeSanaPisteet() {
     "Sait " + pisteet + " pistettä!";
 }
 //2.Tehtävä
-//arvotaan lottonumerot..
-
+/*arvotaan lottonumerot..
+https://stackoverflow.com/questions/37963358/how-can-i-make-a-random-array-with-no-repeats */
+function lototaan() {
+  var numerot = [],
+    tulos = [];
+  //tehdään taulukko jossa numerot 1-40
+  for (var i = 1; i <= 40; i++) {
+    numerot.push(i);
+  }
+  //console.log(numerot);
+  //sekoitetaan taulukko https://www.w3schools.com/js/js_array_sort.asp
+  numerot.sort(function (a, b) {
+    return 0.5 - Math.random();
+  });
+  //console.log(numerot);
+  //otetaan numerot-taulukosta 7 numeroa tulos-taulukkoon
+  for (var j = 0; j < 7; j++) {
+    tulos[j] = numerot[j];
+  }
+  //console.log(tulos);
+  //laitetaan numerot järjesteykseen(pienimmästä isoimpaan) ja poistetaan taulukosta pilkut
+  tulos = tulos.sort((a, b) => a - b);
+  tulos = tulos.join(" ");
+  //console.log(tulos);
+  //kirjoitetaan vastaus htmlään
+  document.getElementById("vastaus2").innerHTML =
+    "Arvotut numerot ovat: " + tulos;
+}
 //3.Tehtävä
 //muunnetaan ennalta määritelty js taulukko html taulukkomuotoon..
 
 //4.Tehtävä
 //tehdään taulukko kaikista korttipakankorteista, arvotaan sieltä 5korttia..
 
+/* Randomi numero funktio..
+https://www.w3schools.com/JS/js_random.asp */
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 //tyhjätään KAIKKI..
 function empty() {
   document.getElementById("scrabbleSana").value = "";
@@ -81,7 +112,7 @@ function empty() {
   //document.getElementById("").value = "";
   //document.getElementById("").value = "";
   document.getElementById("vastaus1").innerHTML = "";
-  //document.getElementById("vastaus2").innerHTML = "";
+  document.getElementById("vastaus2").innerHTML = "";
   //document.getElementById("vastaus3").innerHTML = "";
   //document.getElementById("vastaus4").innerHTML = "";
 }
