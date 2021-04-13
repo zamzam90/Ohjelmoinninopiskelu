0674a5da0be86d0bf9415f0c54522c5b89cb1dbc
zamzam90 <siltanen.sami@hotmail.com>
2021-04-13 19:59:30 +0300

    Erittäin simppeli laskin tehty..

---
+++ random-pikku-projekteja/laskin/js/koodi.js
@@ -0,0 +1,32 @@
+//Das Laskin..
+console.log("Erittäin erittäin simppeli laskin..\nMade by: Sami Siltanen");
+//alustetaan muuttujia:
+var tulos = "",
+  luku = "",
+  operaattorit = ["+", "-", "*", "/", "%"];
+//Näyttö-funktio (tässä näkyy lisätyt luvut / operaattorit / tulos), tulos muuttuja
+function kalkuloi() {
+  tulos = eval(tulos);
+  console.log("tuloste 'jono' tällä hetkellä: " + tulos);
+  document.getElementById("näyttö").innerHTML = tulos;
+}
+//Lisäys-funktio (lisää luvun / operaattorin) lisätään luku tulosmuuttujaan
+function lisää(luku) {
+  tulos += luku;
+  console.log("luku " + luku + " lisätty tulosteeseen"); //for debugging..
+  console.log(tulos);
+  document.getElementById("näyttö").innerHTML = tulos;
+}
+/* ToDo..
+//Poisto-funktio (poistaa viimeksi lisätyn luvun / operaattorin)
+function poista() {
+  console.log("Hello World");
+}
+*/
+//Alustus-funktio (tyhjentää "näytön")
+function alusta() {
+  console.log("Näyttö tyhjennetty.."); //for debugging..
+  tulos = "";
+  tyhjätty = "Klikkaile nappuloita!";
+  document.getElementById("näyttö").innerHTML = tyhjätty;
+}
