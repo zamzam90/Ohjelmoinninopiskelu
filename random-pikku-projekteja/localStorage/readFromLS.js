c011df42d3f0e542c01c907c79adc3ff2ba2c164
zamzam90 <siltanen.sami@hotmail.com>
2021-05-10 14:04:50 +0300

    localStorage tietojen tulostus HTMLään toimii, TODO lisätä avaimen poisto localStoragesta

--- random-pikku-projekteja/localStorage/readFromLS.js
+++ random-pikku-projekteja/localStorage/readFromLS.js
@@ -1,67 +1,49 @@
 console.log("Hello stranger..");
 
-/* 
-TODO:
-  -hakea kaikki tiedot localstoragesta
-  -parse json?
-  -tulostaa tiedot htmlään, avain ok, arvo, objecti, jokainen omaan table soluun?
-  -tietojen(avaimen) poisto mahdollisuus?
-  -???
-  -testailla
-*/
-
 /* alustetaan muuttujia ja haetaan niihin tietoa */
 var infoArea = document.getElementById("infoArea");
 var listaaBtn = document.getElementById("listaaBtn");
 
 /* 
   TODO:
-  0. valitse tietty avain vai näytä kaikkien avainten(hlötietojen) arvo(t)(=tiedot)?
-  1. hae avain/avaimet localstoragesta -> muuttujaan
-  1.1 arvo(t?) -> muuttujaan (JSON.parse)
-  1.2 käy koko arvo objekti läpi ja puske tiedot johonkin muuttujiin
-  2. kirjoita for loopilla htmltablee?:
-      -avain: table header colspan 2
-      -arvot table cell etc?
-      Henkilötiedot 0:
-      Etunimi: Matti
-      Sukunimi: Meikäläinen
-      jne..
+  -printToHTML += vaihtaa htmlDOM menetelmiin...
+  -??
+  -testailla
+  -fixailla/hifistellä..
 */
-
-/* localStorage.key(0) //= henkilötiedot 0
-JSON.parse(localStorage.getItem("henkilötiedot 0")); */
-
-/* testing */
-test = JSON.parse(localStorage.getItem("henkilötiedot 0"));
-function printObject(o) {
-  var out = "";
-  for (var p in o) {
-    out += p + ": " + o[p] + "\n";
-  }
-  document.getElementById("infoArea").innerHTML = out;
-}
-/* helvetisti tekemistä vielä */
 
 /* Funktio joka tulostaa tiedot htmlään */
 function listaa() {
   /* alustetaan muuttujia ja haetaan niihin tietoa */
   var printKey,
     printValue,
-    printHTML = "";
+    arrayFromLocalStorageObject = [],
+    printToHTML = "<table align='center'>";
 
   console.log("tulostetaan...");
   for (x = 0; x < localStorage.length; x++) {
     // hae avain localStoragesta
     printKey = localStorage.key(x);
-    console.log("printKey");
-    // hae avaimella arvo localStoragesta
-    printValue = JSON.parse(localStorage.getItem("printKey"));
+    console.log(printKey);
+    printToHTML += "<tr><th colspan='2'>";
+    printToHTML += printKey;
+    printToHTML += "</th></tr>";
+    // hae avaimella arvo(olio) localStoragesta
+    printValue = JSON.parse(localStorage.getItem(printKey));
     console.log(printValue);
-    // lisätään avain ja sen arvo tulosteeseen
-    printHTML += printKey + " " + printValue + "<br>";
+    // muutetaan olio listaksi https://www.javascripttutorial.net/object/convert-an-object-to-an-array-in-javascript/
+    arrayFromLocalStorageObject = Object.entries(printValue);
+    // tehdään table avaimesta ja arvosta(objektin listasta)
+    for (a = 0; a < arrayFromLocalStorageObject.length; a++) {
+      printToHTML += "<tr>";
+      for (b = 0; b < arrayFromLocalStorageObject[a].length; b++) {
+        printToHTML += "<td>" + arrayFromLocalStorageObject[a][b] + "</td>";
+      }
+      printToHTML += "</tr>";
+    }
   }
-  document.getElementById("infoArea").innerHTML = printHTML;
+  printToHTML += "</table>";
+  document.getElementById("infoArea").innerHTML = printToHTML;
 }
 
 /* Funktio joka näyttää localStoragen tiedot konsolissa*/
