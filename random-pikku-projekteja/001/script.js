1f803eec799f168eb377b717704d31b0b63380aa
zamzam90 <siltanen.sami@hotmail.com>
2021-06-04 19:29:20 +0300

    jotain puuhailua..

---
+++ random-pikku-projekteja/001/script.js
@@ -0,0 +1,8 @@
+/* 
+TODO:
+vaihtaa bodyn taustaväriä:
+    -napista "satunnainen" väri
+    -colorpikkeristä väri
+*/
+const randomColor = document.getElementById("satunnainenVari");
+const manualColor = document.getElementById("manuaalinenVari");
