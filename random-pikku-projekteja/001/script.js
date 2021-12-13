6ccbf1466e1bf77f745db3b8610f963439e1f248
zamzam90 <siltanen.sami@hotmail.com>
2021-12-13 18:49:19 +0200

    jottai pientä taas pitkästä aikaa

--- random-pikku-projekteja/001/script.js
+++ random-pikku-projekteja/001/script.js
@@ -1,8 +1,44 @@
 /* 
 TODO:
-vaihtaa bodyn taustaväriä:
-    -napista "satunnainen" väri
+-vaihtaa containerin taustaväriä:
     -colorpikkeristä väri
+-mobiili kikkailu?
 */
-const randomColor = document.getElementById("satunnainenVari");
+
+const colors = [
+  "#FF0000",
+  "#00FF00",
+  "#0000FF",
+  "#FFFF00",
+  "#FF00FF",
+  "#00FFFF",
+  "#FF7F00",
+  "#FF007F",
+  "#7FFF00",
+  "#7F00FF",
+  "#00FF7F",
+  "#007FFF",
+];
+console.log("värit: " + colors);
+
+//vaihdetaan taustaväriksi joku satunnainen väri colors arraystä
+function randomColor() {
+  var howManyColors = colors.length;
+  var randomNumberForColor = Math.floor(Math.random() * howManyColors);
+  console.log(
+    "random väri: " +
+      randomNumberForColor +
+      " = " +
+      colors[randomNumberForColor]
+  );
+  document.getElementById("tausta").style.backgroundColor =
+    colors[randomNumberForColor];
+}
+
 const manualColor = document.getElementById("manuaalinenVari");
+
+//resetoidaan tausta takaisin alkuperäiselle värille
+function bgColorReset() {
+  document.getElementById("tausta").style.backgroundColor = "#2c3e50";
+  console.log("taustaväri resetoitu..");
+}
